9b175bbc635eaaa5503db572fe6ab946b402a2d6
Tanja Rietveldt <diafox2015@gmail.com>
2019-05-30 20:35:10 +0200

    adjust done and fail ajax method for jquery 3.2 with try\catch blocks

--- bt/static/js/main.js
+++ bt/static/js/main.js
@@ -1,6 +1,5 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
-
 // # do auto disappearance of aler messages
 //function setTimeOut(2 args: func and time==3000 or 3 seconds)
 setTimeout(
@@ -9,7 +8,6 @@ setTimeout(
     },3000);
 
 // csrf_token settings
-
 (function(){
     var csrf_token = Cookies.get('csrftoken');
     console.log('where is my token:',csrf_token);
@@ -19,64 +17,73 @@ setTimeout(
 
 })();
 
-//$(...).load(url,data,function(response,status,xhr))
 $(".need_auth").submit(function(e){
         console.log('Inside need_auth');
         var loginZoo = $("#loginZoo");
-
         e.preventDefault();
          var but_submit = $(this);
          var url = $(this).attr('action');
          var data = $(this).serialize();
-         $.ajax({
+         var request = $.ajax({
             url:url,
-            type:'POST',
-            data:data,
-            success:function(res){
-                //console.log("YEs!!!!!!!!!!!");
-                window.location='/';
-            },
-            error:function(xhr,status,error){
-                console.log('xhr',xhr);
-                console.log(error); //Bad Request
-                console.log(status); // error
-                console.log('xhr state status',xhr.state);
+            method:'POST',
+            data:data
+        })
+        // success
+        request.done(function(data, textStatus, xhr){
+            //console.log(data); //{'html':"",form":{...},location:"/"}
+            //console.log(textStatus); //success
+            //console.log(xhr);        // xhr with all k,v{}
+            window.location='/';
+            }
 
-                var errLogin = xhr.responseJSON.form.errors;
+        );
+        //fail
+        request.fail(function( jqXHR, textStatus ){
+            console.log( "Request failed: " + textStatus );
+            try {
+                var errLogin = jqXHR.responseJSON.form.errors;
                 if (errLogin){
                     loginZoo.html(errLogin);
                     loginZoo.addClass("alert alert-danger");
                 }
+            }
+            catch{
+                console.log("it's not login error");
+            }
+            try{
 
-                //console.log(res);
-                var errMail = xhr.responseJSON.form.fields.email.errors;
-                var errPsw1 = xhr.responseJSON.form.fields.password1.errors;
-                var errPsw2 = xhr.responseJSON.form.fields.password2.errors;
-                var errName = xhr.responseJSON.form.fields.username.errors;
+                var errName = jqXHR.responseJSON.form.fields.username.errors;
+                var errMail = jqXHR.responseJSON.form.fields.email.errors;
+                var errPsw1 = jqXHR.responseJSON.form.fields.password1.errors;
+                var errPsw2 = jqXHR.responseJSON.form.fields.password2.errors;
+                console.log(errName);
+                if(errName){
+                    $("#userName").html(errName);
+                    $("#userName").addClass("alert alert-danger");
+                }
+
                 if(errPsw1){
                     psw1 = $("#psw1");
                     psw1.html(errPsw1);
                     psw1.addClass("alert alert-danger");
-
                 }
                 if(errPsw2){
                     $("#psw2").html(errPsw2);
                     $("#psw2").addClass("alert alert-danger");
                 }
-                if(errName){
-                    $("#userName").html(errName);
-                    $("#userName").addClass("alert alert-danger");
-                }
+
                 if(errMail){
                     $("#mail").html(errMail);
                     $("#mail").addClass("alert alert-danger");
                 }
-
             }
+            catch{
+                console.log("it was not signup call")
+            }
+        }); //end error
 
-        })
-
-        });
+});
 //Option: make another css class instead of built-in
 // bootstrap "alert alert-danger"
         // $('h3').css({
